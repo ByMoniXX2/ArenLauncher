f162323a11a0322cb65c0ca26dec1edc97568ff4
ByMoniXX <olis020926@gmail.com>
2021-05-21 15:22:54 -0500

    Change experimental v1.4.4

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -537,8 +537,8 @@ exports.pullRemote = function(){
         return exports.pullLocal()
     }
     return new Promise((resolve, reject) => {
-        const distroURL = 'https://gist.githubusercontent.com/ByMoniXX2/5cae639ee1b270bfb9b4e1ac57af31d2/raw/bfed193df780d82646504bc1376c5e3500eb6ca8/distribution.json'
-        //const distroURL = 'https://gist.githubusercontent.com/ByMoniXX2/5cae639ee1b270bfb9b4e1ac57af31d2/raw/bfed193df780d82646504bc1376c5e3500eb6ca8/distribution.json'
+        const distroURL = 'https://gist.githubusercontent.com/ByMoniXX2/5cae639ee1b270bfb9b4e1ac57af31d2/raw/836a3704e23b298b12cccf39396ccab8ef018a29/distribution.json'
+        //const distroURL = 'https://gist.githubusercontent.com/ByMoniXX2/5cae639ee1b270bfb9b4e1ac57af31d2/raw/836a3704e23b298b12cccf39396ccab8ef018a29/distribution.json'
         const opts = {
             url: distroURL,
             timeout: 10000
